a058a5dca76f18037c4dce616348b19d6dd350e8
TienIT0U120904 <tientranthi1209@gmail.com>
2025-02-24 14:07:56 +0700

    addcode ftt

--- UnitTestDemo/src/main/java/daihocmo/unittestdemo/UnitTestDemo.java
+++ UnitTestDemo/src/main/java/daihocmo/unittestdemo/UnitTestDemo.java
@@ -12,6 +12,6 @@ public class UnitTestDemo {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.err.println("hudhsd");
+        System.out.println("hudhsd 02");
     }
 }
