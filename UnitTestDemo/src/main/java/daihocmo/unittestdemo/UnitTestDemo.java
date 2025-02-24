e8b137eb12fa1d5d3059272265b27f13f7268a14
nhuquynh <qp7139@gmail.com>
2025-02-24 13:39:48 +0700

    new project

---
+++ UnitTestDemo/src/main/java/daihocmo/unittestdemo/UnitTestDemo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package daihocmo.unittestdemo;
+
+/**
+ *
+ * @author admin
+ */
+public class UnitTestDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
